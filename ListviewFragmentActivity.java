aa0196da92ad11c78552695d38a3a90fe1d76bf8
Ben <bs6171@bard.edu>
2016-09-24 16:12:06 -0400

    Update ListviewFragmentActivity.java

--- ListviewFragmentActivity.java
+++ ListviewFragmentActivity.java
@@ -20,11 +20,18 @@ public class ListviewFragmentActivity extends FragmentActivity{
         setContentView(R.layout.main);
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.myListView);
+        Fragment fragmentOther = fm.findFragmentById(R.id.textView);
         if (fragment == null) {
             fragment = new ToDoListviewFragment();
             fm.beginTransaction()
                     .add(R.id.myListView, fragment)
                     .commit();
+        }
+        if (fragmentOther == null) {
+            fragmentOther == new ItemView();
+            fm.beginTransaction();
+                .add(R.id.textView, fragmentOther);
+                .commit;
         }
 
     }
