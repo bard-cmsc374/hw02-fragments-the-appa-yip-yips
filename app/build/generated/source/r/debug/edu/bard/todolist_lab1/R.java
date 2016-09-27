57b26f52c9fc9405b99ae47e8ff2c12c8e5beaf4
ScholarOfTheFirstWin <bs6171@bard.edu>
2016-09-27 17:22:47 -0400

    completed

--- app/build/generated/source/r/debug/edu/bard/todolist_lab1/R.java
+++ app/build/generated/source/r/debug/edu/bard/todolist_lab1/R.java
@@ -15,11 +15,15 @@ public final class R {
     }
     public static final class id {
         public static final int addButton=0x7f050000;
+        public static final int fragment_container_item=0x7f050003;
+        public static final int fragment_container_list=0x7f050004;
         public static final int myEditText=0x7f050001;
         public static final int myListView=0x7f050002;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int itemfrag=0x7f030000;
+        public static final int listfrag=0x7f030001;
+        public static final int main=0x7f030002;
     }
     public static final class string {
         public static final int add_button=0x7f040000;
